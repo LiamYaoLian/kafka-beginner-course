dc03e9c5657002057c95d0ef59d61b2177646bc6
liamyaolian <liamyaolian@gmail.com>
2022-11-09 14:15:13 -0500

    Wikimedia Producer Project Setup

--- kafka-producer-wikimedia/src/main/java/ind/ll/demo/kafka/wikimedia/WikiMediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/ind/ll/demo/kafka/wikimedia/WikiMediaChangesProducer.java
@@ -1,7 +1,20 @@
 package ind.ll.demo.kafka.wikimedia;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class WikiMediaChangesProducer {
   public static void main(String[] args) {
+    // create producer properties
+    Properties properties = new Properties();
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+    // create the producer
+    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
   }
 }
