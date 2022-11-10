d17e8a4aff880902e49d7dc2a0fed828834c6860
liamyaolian <liamyaolian@gmail.com>
2022-11-10 09:31:25 -0500

    opensearch setup

---
+++ kafka-producer-wikimedia/src/main/java/ind/ll/demo/kafka/wikimedia/WikiMediaChangeHandler.java
@@ -0,0 +1,48 @@
+package ind.ll.demo.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikiMediaChangeHandler implements EventHandler {
+  private KafkaProducer<String, String> kafkaProducer;
+  private String topic;
+  private final Logger log = LoggerFactory.getLogger(WikiMediaChangeHandler.class.getSimpleName());
+
+  public WikiMediaChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+    this.kafkaProducer = kafkaProducer;
+    this.topic = topic;
+  }
+
+
+
+  @Override
+  public void onOpen() {
+    // nothing
+  }
+
+  @Override
+  public void onClosed() {
+    kafkaProducer.close();
+  }
+
+  @Override
+  public void onMessage(String event, MessageEvent messageEvent) {
+    log.info(messageEvent.getData());
+    // async
+    kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+  }
+
+  @Override
+  public void onComment(String comment) {
+    // nothing
+  }
+
+  @Override
+  public void onError(Throwable t) {
+    log.error("Error in stream reading", t);
+  }
+}
--- kafka-producer-wikimedia/src/main/java/ind/ll/demo/kafka/wikimedia/WikiMediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/ind/ll/demo/kafka/wikimedia/WikiMediaChangesProducer.java
@@ -1,20 +1,42 @@
 package ind.ll.demo.kafka.wikimedia;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.net.URI;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class WikiMediaChangesProducer {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     // create producer properties
     Properties properties = new Properties();
     properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
     properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+    // set high throughput producer configs
+    properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+    properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32* 1024));
+    properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
     // create the producer
     KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+    String topic = "wikimediaRecentChange";
+
+    EventHandler eventHandler = new WikiMediaChangeHandler(producer, topic);
+    String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+    EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+    EventSource eventSource = builder.build();
+
+    // start producer in another thread
+    eventSource.start();
+
+    // produce for 10 min and block the program until then
+    TimeUnit.MINUTES.sleep(10);
   }
 }
